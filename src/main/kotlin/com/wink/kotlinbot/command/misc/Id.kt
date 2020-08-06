2a45a55254f68828a72d5499b1cf4e2011e1dff6
Shawn Kaplan <shawn.s.kaplan@gmail.com>
2020-08-06 18:04:10 -0400

    Added id command

---
+++ src/main/kotlin/com/wink/kotlinbot/command/misc/Id.kt
@@ -0,0 +1,40 @@
+package com.wink.kotlinbot.command.misc
+
+import com.jagrosh.jdautilities.command.Command
+import com.jagrosh.jdautilities.command.CommandEvent
+import com.wink.kotlinbot.property.BotProperties
+import com.wink.kotlinbot.service.impl.MessagingService
+import net.dv8tion.jda.api.entities.MessageChannel
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class Id @Autowired constructor(
+        private val messageSender: MessagingService,
+        private val botProperties: BotProperties
+) : Command() {
+
+    init {
+        name = "id"
+        help = "Provides the name of a user with a corresponding ID"
+        arguments = "<ID number of the user>"
+    }
+
+    override fun execute(event: CommandEvent) {
+        val id: Long? = event.args.toLongOrNull()
+        if (id == null) {
+            sendHelp(event.channel)
+            return
+        }
+
+        var out: String? = event.jda.getUserById(id)?.name
+        out = if (out != null) "Found user: $out" else "Could not find a user with that ID"
+        messageSender.sendMessage(event.channel, out)
+    }
+
+    private fun sendHelp(channel: MessageChannel) {
+        val call: String = botProperties.commandPrefix + name
+        val errorOut = "Expected: $call $arguments\nExample: `$call 192882738527731712`"
+        messageSender.sendMessage(channel, errorOut)
+    }
+}

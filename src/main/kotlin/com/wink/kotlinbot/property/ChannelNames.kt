c87c1b08fd4bc5bec4f977baa6916103a374b84b
Shawn Kaplan <shawn.s.kaplan@gmail.com>
2020-08-06 11:50:24 -0400

    Added ChannelNames.kt

---
+++ src/main/kotlin/com/wink/kotlinbot/property/ChannelNames.kt
@@ -0,0 +1,13 @@
+package com.wink.kotlinbot.property
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "channel.name")
+data class ChannelNames(
+        var welcome: String? = null,
+        var deletedMessages: String? = null,
+        var editedMessages: String? = null,
+        var channels: String? = null
+)

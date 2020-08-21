287cfd8fc9f559873324751b4c8981ed27ca1bbc
Shawn Kaplan <shawn.s.kaplan@gmail.com>
2020-08-21 17:09:26 -0400

    made Card.set JvmStatic

--- src/main/kotlin/com/wink/dbse/entity/game/cardgame/card/Card.kt
+++ src/main/kotlin/com/wink/dbse/entity/game/cardgame/card/Card.kt
@@ -5,6 +5,7 @@ class Card(val rank: CardRank, val suit: CardSuit) {
     override fun toString(): String = "[" + rank.symbol + " " + suit.symbol + "]"
 
     companion object {
+        @JvmStatic
         fun set(): Set<Card> {
             val cards = HashSet<Card>()
             for (suit in CardSuit.values()) {
